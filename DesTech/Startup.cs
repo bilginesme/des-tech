590b65dafad6624cbb2bf6cb6da41c3ca40405c2
Bilgin Esme <besme@esme.org>
2017-12-26 22:39:04 +0300

    Add project files.

---
+++ DesTech/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DesTech.Startup))]
+namespace DesTech
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
