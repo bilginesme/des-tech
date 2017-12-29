352c2259d26d19920744d1f95911794969d8a2a1
Bilgin Esme <besme@esme.org>
2017-12-29 19:22:06 +0300

    Skin implemented

--- DesTech/App_Start/BundleConfig.cs
+++ DesTech/App_Start/BundleConfig.cs
@@ -9,7 +9,18 @@ namespace DesTech
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/custom.js",
+                        "~/Scripts/theme.js",
+                        "~/Scripts/jquery.magnific-popup.min.js",
+                        "~/Scripts/wow.min.js",
+                        "~/Scripts/isotope.pkgd.min.js",
+                        "~/Scripts/imagesloaded.pkgd.min.js",
+                        "~/Scripts/smoothscroll.js",
+                        "~/Scripts/jquery.easing.min.js",
+                        "~/Scripts/jquery.mb.YTPlayer.min.js",
+                        "~/Scripts/owl.carousel.min.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,10 +33,19 @@ namespace DesTech
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/custom.css",
+                      "~/Content/theme.css",
+                      "~/Content/animate.min.css",
+                      "~/Content/magnific-popup.css",
+                      "~/Content/jquery.mb.YTPlayer.min.css",
+                      "~/Content/owl.carousel.min.css",
+                      "~/Content/owl.theme.default.css"
+                      ));
+
+            bundles.Add(new StyleBundle("~/font-awesome/css").Include("~/fonts/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
         }
     }
 }
