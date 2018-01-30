b28d1326ca1281ef1e8dc1133902b5b876673a44
Bilgin Esme <besme@esme.org>
2018-01-30 23:23:33 +0300

    Skin Implementation Complete

--- DesTech/App_Start/BundleConfig.cs
+++ DesTech/App_Start/BundleConfig.cs
@@ -10,8 +10,8 @@ namespace DesTech
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/custom.js",
-                        "~/Scripts/theme.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/respond.js",
                         "~/Scripts/jquery.magnific-popup.min.js",
                         "~/Scripts/wow.min.js",
                         "~/Scripts/isotope.pkgd.min.js",
@@ -19,30 +19,27 @@ namespace DesTech
                         "~/Scripts/smoothscroll.js",
                         "~/Scripts/jquery.easing.min.js",
                         "~/Scripts/jquery.mb.YTPlayer.min.js",
-                        "~/Scripts/owl.carousel.min.js"
+                        "~/Scripts/owl.carousel.min.js",
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/custom.js",
+                        "~/Scripts/theme.js"
                         ));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
+            
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
             
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/custom.css",
-                      "~/Content/theme.css",
+                        "~/Content/bootstrap.min.css",
                       "~/Content/animate.min.css",
                       "~/Content/magnific-popup.css",
                       "~/Content/jquery.mb.YTPlayer.min.css",
                       "~/Content/owl.carousel.min.css",
-                      "~/Content/owl.theme.default.css"
+                      "~/Content/owl.theme.default.css",
+                      "~/Content/custom.css",
+                      "~/Content/theme.css",
+                      "~/Content/helper.css"
                       ));
 
             bundles.Add(new StyleBundle("~/font-awesome/css").Include("~/fonts/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
